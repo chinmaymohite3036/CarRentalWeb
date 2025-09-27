b390f7117d10b49ad77fb6e87dd84399ff7e8267
chinmaymohite3036 <chinmaymohite0@gmail.com>
2025-09-27 13:06:22 +0530

    login updated

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Server is running"));
-app.use('/api/user', userRouter);
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
