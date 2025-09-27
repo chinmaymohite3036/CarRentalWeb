6bd7dd7ef7df64e8e7e817201b1dd0c0993ae125
chinmaymohite3036 <chinmaymohite0@gmail.com>
2025-09-27 15:57:28 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -14,8 +14,10 @@ await connectDB();
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/user", userRouter);
+app.use('/api/owner', ownerRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
