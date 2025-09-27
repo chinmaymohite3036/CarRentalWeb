febe39316a155e0799ba170e286b540a2efdc432
chinmaymohite3036 <chinmaymohite0@gmail.com>
2025-09-27 21:46:04 +0530

    updated working imagekit

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,4 +1,3 @@
-import { format } from "path";
 import imagekit from "../configs/imageKit.js";
 import User from "../models/User.js";
 import fs from "fs";
@@ -6,49 +5,118 @@ import Car from "../models/Car.js";
 
 // API to change role of user
 export const changeRoleToOwner = async (req, res) => {
-    try {
-        const {_id} = req.user;
-        await User.findByIdAndUpdate(_id, {role: "owner"})
-        res.json({success: true, message: "Now you are an owner"})
-    } catch(error){
-        console.log(error.message);
-        res.json({success: false, message: error.message})
-    }
-}
+  try {
+    const { _id } = req.user;
+    await User.findByIdAndUpdate(_id, { role: "owner" });
+    res.json({ success: true, message: "Now you are an owner" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // API to list car
+export const addCar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    let car = JSON.parse(req.body.carData);
+    const imageFile = req.file;
 
-export const addCar = async (req, res)=>{
-    try{
-        const {_id} = req.user;
-        let car = JSON.parse(req.body.carData);
-        const imageFile = req.file;
+    // Upload image to imageKit
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/cars",
+    });
 
-        // Upload image to imageKit
-        const fileBuffer =fs.readFileSync(imageFile.path)
-        const response = await imagekit.upload({
-            file: fileBuffer,
-            fileName: imageFile.originalname,
-            folder: '/cars'
-        })
+    // optimization through imagekit URL transformation
+    var optimizedImageUrl = imagekit.url({
+      path: response.filePath, // Corrected from .filepath
+      transformation: [
+        { width: 1280 }, // Corrected from string '1280' to number
+        { quality: "auto" },
+        { format: "webp" },
+      ],
+    });
 
-        // optimization through imagekit URL transformation
-        var optimizedImageUrl = imagekit.url({
-            path: response.filepath,
-            transformation: [
-                {width: '1280'},  // width resizing
-                {quality: 'auto'},  // auto compression
-                {format : 'webp'}  // convert to modern format
-            ]
-        });
+    const image = optimizedImageUrl;
+    await Car.create({ ...car, owner: _id, image });
 
-        const image = optimizedImageUrl;
-        await Car.create({...car, owner: _id, image})
+    res.json({ success: true, message: "Car added successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-        res.json({success: true, message: "Car added successfully"})
+// API to list owner cars
+export const getOwnerCars = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const cars = await Car.find({ owner: _id });
+    res.json({ success: true, cars });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-    }catch(error){
-        console.log(error.message);
-        res.json({success: false, message: error.message})
+// API to toggle car availability
+export const toggleCarAvailability = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { carId } = req.body;
+    const car = await Car.findById(carId);
+
+    // Checking is car belongs to the user
+    if (car.owner.toString() !== _id.toString()) {
+      return res.json({ success: false, message: "Unauthorized" });
     }
-}
+
+    car.isAvaliable = !car.isAvaliable;
+    await car.save();
+
+    res.json({ success: true, message: "Availability Toggled" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to delete a car
+export const deletecar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { carId } = req.body;
+    const car = await Car.findById(carId);
+
+    // Checking is car belongs to the user
+    if (car.owner.toString() !== _id.toString()) {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+
+    car.owner = null;
+    car.isAvaliable = false;
+    await car.save();
+
+    res.json({ success: true, message: "Car Removed" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get dashboard data
+export const getDashboardData = async (req, res) => {
+  try {
+    const { _id, role } = req.user;
+
+    if (role !== "owner") {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
