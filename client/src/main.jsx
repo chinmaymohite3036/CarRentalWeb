164c9499ffdcbed8777e2d79140633bc9209de8b
chinmaymohite3036 <chinmaymohite0@gmail.com>
2025-09-29 02:09:09 +0530

    update main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
+// client/src/main.jsx
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { AppProvider } from "./context/AppContext.jsx";
+import AppProvider from "./context/AppContext.jsx"; // This line is now correct
 import { MotionConfig } from "motion/react";
 
 createRoot(document.getElementById("root")).render(
