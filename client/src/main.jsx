1c50906645885b0273449de2d4ba8ed04330a501
chinmaymohite3036 <chinmaymohite0@gmail.com>
2025-09-29 02:14:33 +0530

    updated main1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,17 @@
-// client/src/main.jsx
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import AppProvider from "./context/AppContext.jsx"; // This line is now correct
+import { AppProvider } from "./context/AppContext.jsx"; 
 import { MotionConfig } from "motion/react";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AppProvider>
       <MotionConfig viewport={{ once: true }}>
         <App />
+        <Toaster />
       </MotionConfig>
     </AppProvider>
   </BrowserRouter>
